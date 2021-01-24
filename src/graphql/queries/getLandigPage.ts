e2b6792544b1f879d9ebb18fcbbfb338ffdc6d31
CriptX-42 <ricardo-svc@live.com>
2021-01-24 19:54:44 -0300

    Fazendo ligações da parte de 'sectionFaq'

--- src/graphql/queries/getLandigPage.ts
+++ src/graphql/queries/getLandigPage.ts
@@ -108,6 +108,16 @@ const GET_LANDING_PAGE = /* GraphQL */ `
     }
   }
 
+  fragment sectionFaq on LandingPage {
+    sectionFaq {
+      title
+      questions {
+        question
+        answer
+      }
+    }
+  }
+
   query GET_LANDING_PAGE {
     landingPage {
       ...logo
@@ -120,6 +130,7 @@ const GET_LANDING_PAGE = /* GraphQL */ `
       ...pricingBox
       ...sectionAboutUs
       ...sectionReview
+      ...sectionFaq
     }
   }
 `
