f770047b0cd081ad00d0d6648859c3dc166401ef
CriptX-42 <ricardo-svc@live.com>
2021-01-24 19:14:54 -0300

    Fazendo ligações da parte de 'sectionAboutUs'

--- src/types/api.ts
+++ src/types/api.ts
@@ -5,6 +5,12 @@ export type TechIcon = {
   }
 }
 
+export type Image = {
+  alternativeText: string
+  url: string
+}
+
+
 export type LogoProps = {
   alternativeText: 'React Avançado'
   url: '/uploads/logo_74e2fdc755.svg'
@@ -69,6 +75,24 @@ export type PricingBoxProps = {
   }
 }
 
+export type SocialLink = {
+  title: string
+  url: string
+}
+
+export type Author = {
+  photo: Image
+  name: string
+  role: string
+  socialLinks: SocialLink[]
+  description: string
+}
+
+export type SectionAboutUsProps = {
+  title: string
+  authors: Author[]
+}
+
 export type LandingPageProps = {
   logo: LogoProps
   header: HeaderProps
@@ -78,4 +102,5 @@ export type LandingPageProps = {
   sectionModules: SectionModulesProps
   sectionAgenda: SectionAgendaProps
   pricingBox: PricingBoxProps
+  sectionAboutUs: SectionAboutUsProps
 }
